b86038ed40977784c9115969e79c1c16ff8afc13
AlekseiKorotkov159 <158587440+AlekseiKorotkov159@users.noreply.github.com>
2024-08-17 19:55:00 +0300

    fix running

--- src/main/java/telran/net/TcpClientServerSession.java
+++ src/main/java/telran/net/TcpClientServerSession.java
@@ -2,13 +2,16 @@ package telran.net;
 
 import java.net.*;
 import java.io.*;
-
-public class TcpClientServerSession {
+import static telran.net.TcpConfigurationProperties.*;
+public class TcpClientServerSession implements Runnable{
 	Socket socket;
 	Protocol protocol;
-
-	public TcpClientServerSession(Socket socket, Protocol protocol) {
+	TcpServer tcpServer;
+	public TcpClientServerSession(Socket socket, Protocol protocol, TcpServer tcpServer) {
 		this.socket = socket;
+		this.tcpServer = tcpServer;
+		//using the method setSoTimeout and some solution for getting session to know about shutdown
+		//you should stop the thread after shutdown command
 		this.protocol = protocol;
 	}
 	public void run() {
@@ -16,10 +19,26 @@ public class TcpClientServerSession {
 				new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintStream sender = new PrintStream(socket.getOutputStream())){
 			String line = null;
-			while((line = receiver.readLine()) != null) {
-				String responseStr = protocol.getResponseWithJSON(line);
-				sender.println(responseStr);
+			boolean sessionRunning = true;
+			socket.setSoTimeout(SOCKET_TIMEOUT);
+			long idleTime = 0;
+			while(tcpServer.running && sessionRunning) {
+				try {
+					line = receiver.readLine();
+					if (line == null) {
+						break;
+					}
+					String responseStr = protocol.getResponseWithJSON(line);
+					sender.println(responseStr);
+					idleTime = 0;
+				} catch (SocketTimeoutException e) {
+					idleTime += SOCKET_TIMEOUT;
+					if (idleTime > SESSION_IDLE_TIMEOUT) {
+						sessionRunning = false;
+					}
+				}
 			}
+			socket.close();
 			
 		} catch (Exception e) {
 			System.out.println(e);
--- src/main/java/telran/net/TcpServer.java
+++ src/main/java/telran/net/TcpServer.java
@@ -1,21 +1,53 @@
 package telran.net;
 import java.net.*;
-public class TcpServer {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import static telran.net.TcpConfigurationProperties.*;
+public class TcpServer implements Runnable{
 	Protocol protocol;
 	int port;
+	boolean running = true;
+	ExecutorService executor;
 	public TcpServer(Protocol protocol, int port) {
 		this.protocol = protocol;
 		this.port = port;
+		executor = Executors.newFixedThreadPool(getNumberOfThreads());
+	}
+	private int getNumberOfThreads() {
+		
+		return Runtime.getRuntime().availableProcessors();
+	}
+	public void shutdown() {
+		executor.shutdownNow();
+		running = false;
+		try {
+			executor.awaitTermination(MAX_WAITING_TIME_IN_SECONDS, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			//No interruptions
+		}
+		
 	}
 	public void run() {
 		try(ServerSocket serverSocket = new ServerSocket(port)){
+			// using ServerSocket method setSoTimeout for unblocking "accept" 
+			//otherwise there is case of forever waiting on "accept" method
 			System.out.println("Server is listening on port " + port);
-			while(true) {
-				Socket socket = serverSocket.accept();
-				TcpClientServerSession session =
-						new TcpClientServerSession(socket, protocol);
-				session.run();
+			serverSocket.setSoTimeout(SOCKET_TIMEOUT);
+			while(running) {
+				try {
+					Socket socket = serverSocket.accept();
+
+					TcpClientServerSession session =
+							new TcpClientServerSession(socket, protocol, this);
+					executor.execute(session);
+				} catch (SocketTimeoutException e) {
+					
+				}
+				
 			}
+			
 				
 			
 		} catch(Exception e) {
